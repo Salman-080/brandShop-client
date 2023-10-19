57a928b72b596ffb5b7c29c3f9ac144d72102a87
Md. Salman Siddique <113790578+Salman-080@users.noreply.github.com>
2023-10-20 05:59:50 +0600

    cart page product data dynamically added and some layout, design optimized

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,11 +1,42 @@
 import { useLoaderData } from "react-router-dom";
 
 const Cart = () => {
-    const cartData=useLoaderData();
-    console.log(cartData)
+    const cartDatas = useLoaderData();
+    console.log(cartDatas)
     return (
-        <div>
-            <h2>cart</h2>
+        <div className="max-w-screen-xl mx-auto flex flex-col gap-4">
+            {
+                cartDatas.map(cartData =>
+                    <div key={cartData._id}>
+                        <div  className=" md:flex gap-5 items-center">
+
+                            <div className="md:h-[200px] md:w-[250px]">
+                                <img className="h-full w-full" src={cartData.image} alt="" />
+                            </div>
+
+                            <div className="space-y-3">
+
+                                <h2 className="text-3xl font-semibold">{cartData.productName}</h2>
+                                <h2 className="text-lg">{cartData.brandName}</h2>
+                                <h2 className="text-sm text-gray-500">{cartData.productType}</h2>
+
+                                <button className="btn btn-error">Remove</button>
+
+
+
+                            </div>
+                            <div className="md:flex-grow"></div>
+                            <div className="md:mr-36">
+                                <p className=""><span className="text-xl font-bold">Price:</span> <span className="text-lg ">{cartData.price}$</span> </p>
+                            </div>
+
+
+
+                        </div>
+                        <div className="border border-gray-300 mt-3"></div>
+                    </div>
+                )
+            }
         </div>
     );
 };
